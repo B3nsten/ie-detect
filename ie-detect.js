f1baf0d15d34d7482acfd5fe3fd68dad4f940219
rwarasaurus <info@kieronwilson.co.uk>
2012-08-29 15:45:05 +0100

    clean up

--- ie-detect.js
+++ ie-detect.js
@@ -1,45 +1,33 @@
 (function() {
 	// get ie version return undefined for no ie
 	var ie = (function(){
-		var u, v = 3, d = document.createElement('div'), a = d.getElementsByTagName('i');
-		while (d.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->', a[0]);
-		return v > 4 ? v : u;
+		var undef, version = 3, element = document.createElement('div'), test = element.getElementsByTagName('i');
+		while(element.innerHTML = '<!--[if gt IE ' + (++version) + ']><i></i><![endif]-->', test[0]);
+		return version > 4 ? version : undef;
 	}());
 
-	var $trim = function(str){
+	var trim = function(str){
 		return str.replace(/^\s+|\s+$/g, '');
 	};
 
-	var $clean = function(str){
-		return $trim(str.replace(/\s+/g, ' '));
+	var clean = function(str){
+		return trim(str.replace(/\s+/g, ' '));
 	};
 
-	var b = document.getElementsByTagName('html')[0],
-		c = (ie) ? 'ie' + ie : 'no-ie',
-		s = (b.className.length) ? $clean(b.className).split(' ') : [];
+	var search = document.getElementsByTagName('html');
 
-	// no body tag
-	if(b === undefined) {
-		return;
-	}
+	// no html tag
+	if( ! search.length) return;
 
-	// no class names
-	if(!s.length) {
-		b.className = c;
-		return;
-	}
+	var html = search[0], classname = (ie) ? 'ie' + ie : 'no-ie',
+		classes = (html.className.length) ? clean(html.className).split(' ') : [];
 
 	// remove no-js
-	for(var i = 0; i < s.length; i += 1) {
-		if(s[i] == 'no-js') {
-			s.splice(i, 1);
-			break;
-		}
-	}
+	for(var i = 0; i < s.length; i += 1) if(s[i] == 'no-js') s.splice(i, 1); break;
 
 	// add ie
-	s.push(c);
+	classes.push(classname);
 
 	// set body class names
-	b.className = s.join(' ');
+	html.className = classes.join(' ');
 }());
